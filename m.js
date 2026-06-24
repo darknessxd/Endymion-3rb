1a1996b643b98e934a917b3866fff9b59b39ff34
darknessxd <38543215+darknessxd@users.noreply.github.com>
2026-06-24 19:13:13 +0300

    Update m.js

--- m.js
+++ m.js
@@ -5659,6 +5659,7 @@
       this.createRGBset();
       this.mouseTracker();
       this.cells();
+      this.partyCells();
       this.commands();
       _0x34f3bb.cleaner();
       this.ctx.restore();
